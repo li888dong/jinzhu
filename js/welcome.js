15bfcaeb82d38f2650d2ec77c9bb560f9363ae59
lidong <lid@linkstec.com>
2019-01-15 21:04:34 +0800

    update

--- js/welcome.js
+++ js/welcome.js
@@ -1,11 +1,16 @@
 var welcomeEle = document.getElementById('welcome');
 var containerEle = document.getElementsByClassName('container')[0];
-var toggleWelcomTimer = setTimeout(toggleWelcome,3000);
+var toggleWelcomTimer = setTimeout(toggleWelcome,10000);
 
 function toggleWelcome() {
     clearTimeout(toggleWelcomTimer);
+    game.startGame();
     welcomeEle.style.opacity = 0;
     welcomeEle.zIndex = -1;
     containerEle.style.opacity = 1;
 }
 
+welcomeEle.style.width = window.innerWidth + 'px';
+welcomeEle.style.height = window.innerHeight + 'px';
+containerEle.style.width = window.innerWidth + 'px';
+containerEle.style.height = window.innerHeight + 'px';
